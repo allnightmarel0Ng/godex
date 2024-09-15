c86e86cdee252125723f8af286caae4736139780
allnighmarel0Ng <den.vasiliev65@gmail.com>
2024-09-15 21:52:53 +0300

    add postgres wrapper

---
+++ internal/infrastructure/postgres/database.go
@@ -0,0 +1,41 @@
+package postgres
+
+import (
+	"database/sql"
+	_ "github.com/lib/pq"
+)
+
+type Database struct {
+	db *sql.DB
+}
+
+func NewDatabase(connStr string) (*Database, error) {
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return &Database{
+		db: db,
+	}, nil
+}
+
+func (d *Database) Close() error {
+	return d.db.Close()
+}
+
+func (d *Database) Query(query string, args ...interface{}) (*sql.Rows, error) {
+	return d.db.Query(query, args...)
+}
+
+func (d *Database) Exec(query string, args ...interface{}) (sql.Result, error) {
+	return d.db.Exec(query, args...)
+}
+
+func (d *Database) QueryRow(query string, args ...interface{}) *sql.Row {
+	return d.db.QueryRow(query, args...)
+}
