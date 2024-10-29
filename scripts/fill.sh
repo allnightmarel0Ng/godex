6661b284564b0bac450f756b9993ba58eb26b006
allnightmarel0Ng <den.vasiliev65@gmail.com>
2024-10-29 19:30:11 +0300

    filling script was adjusted

--- scripts/fill.sh
+++ scripts/fill.sh
@@ -1,104 +1,75 @@
 #!/bin/bash
 
-# POST request to http://localhost:8080/store
 curl -X POST http://localhost:8080/store \
      -H "Content-Type: application/json" \
      -d '{
            "link": "https://raw.githubusercontent.com/golang/go/master/src/net/http/http.go"
          }'
-
-# POST request to http://localhost:8080/store
 curl -X POST http://localhost:8080/store \
      -H "Content-Type: application/json" \
      -d '{
            "link": "https://raw.githubusercontent.com/golang/go/master/src/bufio/bufio.go"
          }'
-
-# POST request to http://localhost:8080/store
 curl -X POST http://localhost:8080/store \
      -H "Content-Type: application/json" \
      -d '{
            "link": "https://raw.githubusercontent.com/golang/go/master/src/compress/bzip2/bzip2.go"
          }'
-
-# POST request to http://localhost:8080/store
 curl -X POST http://localhost:8080/store \
      -H "Content-Type: application/json" \
      -d '{
            "link": "https://raw.githubusercontent.com/golang/go/master/src/compress/flate/deflate.go"
          }'
-
-# POST request to http://localhost:8080/store
 curl -X POST http://localhost:8080/store \
      -H "Content-Type: application/json" \
      -d '{
            "link": "https://raw.githubusercontent.com/golang/go/master/src/compress/lzw/writer.go"
          }'
-
-# POST request to http://localhost:8080/store
 curl -X POST http://localhost:8080/store \
      -H "Content-Type: application/json" \
      -d '{
            "link": "https://raw.githubusercontent.com/golang/go/master/src/compress/zlib/reader.go"
          }'
-
-# POST request to http://localhost:8080/store
 curl -X POST http://localhost:8080/store \
      -H "Content-Type: application/json" \
      -d '{
            "link": "https://raw.githubusercontent.com/golang/go/master/src/container/heap/heap.go"
          }'
-
-# POST request to http://localhost:8080/store
 curl -X POST http://localhost:8080/store \
      -H "Content-Type: application/json" \
      -d '{
            "link": "https://raw.githubusercontent.com/golang/go/master/src/fmt/print.go"
          }'
-
-# POST request to http://localhost:8080/store
 curl -X POST http://localhost:8080/store \
      -H "Content-Type: application/json" \
      -d '{
            "link": "https://raw.githubusercontent.com/golang/go/master/src/os/os.go"
          }'
-
-# POST request to http://localhost:8080/store
 curl -X POST http://localhost:8080/store \
      -H "Content-Type: application/json" \
      -d '{
            "link": "https://raw.githubusercontent.com/golang/go/master/src/io/io.go"
          }'
-
-# POST request to http://localhost:8080/store
 curl -X POST http://localhost:8080/store \
      -H "Content-Type: application/json" \
      -d '{
            "link": "https://raw.githubusercontent.com/golang/go/master/src/strings/strings.go"
          }'
-
-# POST request to http://localhost:8080/store
 curl -X POST http://localhost:8080/store \
      -H "Content-Type: application/json" \
      -d '{
            "link": "https://raw.githubusercontent.com/golang/go/master/src/time/time.go"
          }'
-
-# POST request to http://localhost:8080/store
 curl -X POST http://localhost:8080/store \
      -H "Content-Type: application/json" \
      -d '{
            "link": "https://raw.githubusercontent.com/golang/go/master/src/sync/mutex.go"
          }'
-
-# POST request to http://localhost:8080/store
 curl -X POST http://localhost:8080/store \
      -H "Content-Type: application/json" \
      -d '{
            "link": "https://raw.githubusercontent.com/golang/go/master/src/math/abs.go"
          }'
-
-# POST request to http://localhost:8080/store
 curl -X POST http://localhost:8080/store \
      -H "Content-Type: application/json" \
      -d '{
