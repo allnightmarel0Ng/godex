a88557e5c60040e3dc4780d716e7a9f2e983e9c5
allnightmarel0Ng <den.vasiliev65@gmail.com>
2024-11-13 02:08:35 +0300

    structs for use in api

---
+++ internal/domain/model/api.go
@@ -0,0 +1,14 @@
+package model
+
+type Response struct {
+	Code int `json:"code"`
+	Message string `json:"message"`
+}
+
+type Link struct {
+	Link string `json:"link"`
+}
+
+type Signature struct {
+	Signature string `json:"signature"`
+}
